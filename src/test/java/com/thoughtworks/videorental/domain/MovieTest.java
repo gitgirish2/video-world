49aeab0a54e2947befa0897cc892ffdb4e6cca9d
Anil Kumar Kodi <anilkumk@thoughtworks>
2015-04-17 14:38:57 +0530

    added new release movie

---
+++ src/test/java/com/thoughtworks/videorental/domain/MovieTest.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.videorental.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MovieTest {
+
+    @Test
+    public void testGetTitle() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCharge() throws Exception {
+
+    }
+}
